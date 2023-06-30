f897d485356050a31080e3de694f29e58ae75c47
yashRanjan2510 <yashranjan259002@gmail.com>
2023-06-30 07:19:41 +0530

    second commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 module.exports= async ()=>{
-    const mongouri=""
+    const mongouri="mongodb+srv://1903480130064it:Xr0v4oY8spx1JOJ9@cluster0.js53463.mongodb.net/?retryWrites=true&w=majority"
     try{
        await mongoose.connect(mongouri,{
             useUnifiedTopology:true,
